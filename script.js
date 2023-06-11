7a4863c1c8054725e199420270c899fc8ab160f1
Aditya Bisht <adityabisht151@gmail.com>
2023-06-12 00:32:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,96 @@
-let weather = {
-  apiKey: "5b47c3f40d1e22ba16d5c99894178e9f",
-  fetchWeather: function (city) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
-        this.apiKey
-    )
-      .then((response) => {
-        if (!response.ok) {
-          // alert("Sorry! No weather found.");
-    
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    /* Add your CSS styles here */
+  </style>
+</head>
+<body>
+  <div class="weather-container">
+    <h2 class="city"></h2>
+    <div class="weather-details">
+      <img class="icon" src="" alt="">
+      <p class="description"></p>
+      <p class="temp"></p>
+      <p class="humidity"></p>
+      <p class="wind"></p>
+    </div>
+  </div>
+
+  <div class="product-recommendations">
+    <h3>Product Recommendations</h3>
+    <div id="product-list"></div>
+  </div>
+
+  <div class="popup">
+    <div class="popup-content">
+      <h2>No weather found</h2>
+      <p>Please enter a valid city name.</p>
+      <button id="dismiss-popup-btn">Dismiss</button>
+    </div>
+  </div>
+
+  <script>
+    let weather = {
+      apiKey: "5b47c3f40d1e22ba16d5c99894178e9f",
+      fetchWeather: function (city) {
+        fetch(
+          "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+        )
+        .then((response) => {
+          if (!response.ok) {
             document.getElementsByClassName("popup")[0].classList.add("active");
-        
-           
-         
-          // for fancy alert using swalfire function in sweetalert2 plugins and bootstrap
-          // Swal.fire({
-          //   title: "Oops... No weather found",
-          //   text: "Please Enter a valid city name",
-          //   icon: "error",
-          //   buttonsStyling: false,
-          //   confirmButtonText: "OK",
-          //   customClass: {
-          //     confirmButton: "btn btn-primary",
-          //   },
-          // });
-          throw new Error("No weather found.");
-        }
-        return response.json();
-      })
-      .then((data) => this.displayWeather(data));
-  },
-  displayWeather: function (data) {
-    const { name } = data;
-    const { country } = data.sys;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
-    document.querySelector(".city").innerText = name + ", " + country;
-    document.querySelector(".icon").src =
-      "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = temp + "°C";
-    document.querySelector(".humidity").innerText =
-      "Humidity: " + humidity + "%";
-    document.querySelector(".wind").innerText =
-      "Wind speed: " + speed + " km/h";
-    document.querySelector(".weather").classList.remove("loading");
-    document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x1200/?" + description + "')";
-  },
-  search: function () {
-    this.fetchWeather(document.querySelector(".search-bar").value);
-  },
-};
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          this.displayWeather(data);
+          this.fetchProductRecommendations(data.weather[0].description);
+        })
+        .catch((error) => {
+          console.error("Error fetching weather:", error);
+        });
+      },
+      displayWeather: function (data) {
+        const { name } = data;
+        const { country } = data.sys;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = name + ", " + country;
+        document.querySelector(".icon").src =
+          "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText =
+          "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText =
+          "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather-container").classList.remove("loading");
+        document.body.style.backgroundImage =
+          "url('https://source.unsplash.com/1600x1200/?" + description + "')";
+      },
+      fetchProductRecommendations: function (weatherDescription) {
+        // Replace 'YOUR_API_ENDPOINT' with the actual endpoint for your backend server or proxy
+        const apiEndpoint = 'YOUR_API_ENDPOINT';
 
-document.querySelector(".search button").addEventListener("click", function () {
-  weather.search();
-});
-
-document
-  .querySelector(".search-bar")
-  .addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-      weather.search();
-    }
-  });
-
-weather.fetchWeather("Delhi");
-
-document.getElementById("dismiss-popup-btn").addEventListener("click",function(){
-  document.getElementsByClassName("popup")[0].classList.remove("active");
-});
+        // Make an API call to your backend server or proxy to retrieve Amazon product recommendations based on the weather description
+        fetch(apiEndpoint + '?weather=' + weatherDescription)
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Error fetching product recommendations.");
+            }
+            return response.json();
+          })
+          .then((data) => {
+            this.displayProductRecommendations(data);
+          })
+          .catch((error) => {
+            console.error("Error fetching product recommendations:", error);
+          });
+      },
+      display
